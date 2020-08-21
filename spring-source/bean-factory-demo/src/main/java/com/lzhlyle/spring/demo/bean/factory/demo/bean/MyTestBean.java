50f8701dae0df35d7e4921d1e9fda16266c5de78
lzhlyle <lzhlyle@msn.cn>
2020-08-22 00:17:18 +0800

    Add module: bean-factory-demo.

---
+++ spring-source/bean-factory-demo/src/main/java/com/lzhlyle/spring/demo/bean/factory/demo/bean/MyTestBean.java
@@ -0,0 +1,25 @@
+package com.lzhlyle.spring.demo.bean.factory.demo.bean;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+@Slf4j
+public class MyTestBean {
+    private String testStr = "testStr";
+
+    public String getTestStr() {
+        return testStr;
+    }
+
+    public void setTestStr(String testStr) {
+        this.testStr = testStr;
+    }
+
+    public static void main(String[] args) {
+        BeanFactory bf = new XmlBeanFactory(new ClassPathResource("beanFactoryTest.xml"));
+        MyTestBean bean = (MyTestBean) bf.getBean("myTestBean");
+        log.info(bean.getTestStr());
+    }
+}
