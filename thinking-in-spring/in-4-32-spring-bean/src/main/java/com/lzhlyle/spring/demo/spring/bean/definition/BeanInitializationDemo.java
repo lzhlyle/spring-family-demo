6e5ff42ea3645ede1ee26c9ec926f19bf9fe595b
lzhlyle <lzhlyle@msn.cn>
2020-09-03 23:57:06 +0800

    Add @Lazy in BeanInitializationDemo.

--- thinking-in-spring/in-4-32-spring-bean/src/main/java/com/lzhlyle/spring/demo/spring/bean/definition/BeanInitializationDemo.java
+++ thinking-in-spring/in-4-32-spring-bean/src/main/java/com/lzhlyle/spring/demo/spring/bean/definition/BeanInitializationDemo.java
@@ -5,6 +5,7 @@ import com.lzhlyle.spring.demo.spring.bean.definition.factory.IUserFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 @Configuration
 public class BeanInitializationDemo {
@@ -12,12 +13,17 @@ public class BeanInitializationDemo {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
         applicationContext.register(BeanInitializationDemo.class);
         applicationContext.refresh();
+        System.out.println("Spring refreshed.");
+
         IUserFactory userFactory = applicationContext.getBean(IUserFactory.class);
         // 会调用 DefaultUserFactory::init
+
+        System.out.println(userFactory);
         applicationContext.close();
     }
 
     @Bean(initMethod = "initByCustomWithAnnotation")
+    @Lazy
     public DefaultUserFactory userFactory() {
         return new DefaultUserFactory();
     }
