0ca90314e6c63c1806238d0c646a33475faaaada
lzhlyle <lzhlyle@msn.cn>
2020-09-04 14:05:11 +0800

    Append demo to module: in-4-32-spring-bean
    - BeanDestroyDemo

--- thinking-in-spring/in-4-32-spring-bean/src/main/java/com/lzhlyle/spring/demo/spring/bean/definition/BeanDestroyDemo.java
+++ thinking-in-spring/in-4-32-spring-bean/src/main/java/com/lzhlyle/spring/demo/spring/bean/definition/BeanDestroyDemo.java
@@ -4,26 +4,20 @@ import com.lzhlyle.spring.demo.spring.bean.definition.factory.DefaultUserFactory
 import com.lzhlyle.spring.demo.spring.bean.definition.factory.IUserFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 
-@Configuration
-public class BeanInitializationDemo {
+public class BeanDestroyDemo {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
-        applicationContext.register(BeanInitializationDemo.class);
+        applicationContext.register(BeanDestroyDemo.class);
         applicationContext.refresh();
-        System.out.println("Spring refreshed.");
-
         IUserFactory userFactory = applicationContext.getBean(IUserFactory.class);
-        // 会调用 DefaultUserFactory::init
-
-        System.out.println(userFactory);
+        System.out.println("Spring ready to close.");
         applicationContext.close();
+        System.out.println("Spring closed.");
     }
 
-    @Bean(initMethod = "initByCustom")
-    @Lazy
+    @Bean(destroyMethod = "destroyByCustom")
     public DefaultUserFactory userFactory() {
         return new DefaultUserFactory();
     }
--- thinking-in-spring/in-4-32-spring-bean/src/main/java/com/lzhlyle/spring/demo/spring/bean/definition/factory/DefaultUserFactory.java
+++ thinking-in-spring/in-4-32-spring-bean/src/main/java/com/lzhlyle/spring/demo/spring/bean/definition/factory/DefaultUserFactory.java
@@ -1,21 +1,37 @@
 package com.lzhlyle.spring.demo.spring.bean.definition.factory;
 
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
-public class DefaultUserFactory implements IUserFactory, InitializingBean {
+public class DefaultUserFactory implements IUserFactory, InitializingBean, DisposableBean {
     @PostConstruct
     public void initByPostConstruct() {
         System.out.println("@PostConstruct: UserFactory Initializing...");
     }
 
-    public void initByCustomWithAnnotation() {
-        System.out.println("Custom Annotation: UserFactory Initializing...");
+    public void initByCustom() {
+        System.out.println("Custom init: UserFactory Initializing...");
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        System.out.println("afterPropertiesSet: UserFactory Initializing...");
+        System.out.println("InitializingBean.afterPropertiesSet(): UserFactory Initializing...");
+    }
+
+    @PreDestroy
+    public void destroyByPreDestroy() {
+        System.out.println("@PreDestroy: UserFactory Destroying...");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("DisposableBean.destroy(): UserFactory Destroying...");
+    }
+
+    public void destroyByCustom() {
+        System.out.println("Custom destroy: UserFactory Destroying...");
     }
 }
