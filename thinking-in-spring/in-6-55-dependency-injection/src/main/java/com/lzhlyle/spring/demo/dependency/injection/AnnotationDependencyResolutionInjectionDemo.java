3561d9b39b88f057cbbca66665276f84ad51ac0c
lzhlyle <lzhlyle@msn.cn>
2020-09-07 18:21:38 +0800

    Append demo to module in-6-55-dependency-injection
    - AnnotationDependencyResolutionInjectionDemo

---
+++ thinking-in-spring/in-6-55-dependency-injection/src/main/java/com/lzhlyle/spring/demo/dependency/injection/AnnotationDependencyResolutionInjectionDemo.java
@@ -0,0 +1,53 @@
+package com.lzhlyle.spring.demo.dependency.injection;
+
+import com.lzhlyle.spring.demo.ioc.overview.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.support.BeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Lazy;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class AnnotationDependencyResolutionInjectionDemo {
+    @Autowired
+    @Lazy
+    private User lazyUser;
+
+    @Autowired
+    private User user;
+    // DependencyDescriptor:
+    // 必须 eager = true
+    // 实时 required = true
+    // 通过类型 User.class 依赖查找
+    // 字段名称 "user"
+    // 首要 primary = true
+
+    @Autowired
+    private Map<String, User> users;
+
+    @Autowired
+    private Optional<User> userOptional;
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+
+        BeanDefinitionReader reader = new XmlBeanDefinitionReader(applicationContext);
+        String xmlResourcePath = "META-INF/dependency-lookup-context.xml";
+        reader.loadBeanDefinitions(xmlResourcePath);
+
+        applicationContext.register(AnnotationDependencyResolutionInjectionDemo.class);
+        applicationContext.refresh();
+
+        AnnotationDependencyResolutionInjectionDemo demo =
+                applicationContext.getBean(AnnotationDependencyResolutionInjectionDemo.class);
+
+        System.out.println(demo.lazyUser);
+        System.out.println(demo.user);
+        System.out.println(demo.users);
+        System.out.println(demo.userOptional);
+
+        applicationContext.close();
+    }
+}
