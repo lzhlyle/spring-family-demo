ffc06272cd21a1eb481dd3fbdbcb69e491336273
lzhlyle <lzhlyle@msn.cn>
2020-09-08 22:33:31 +0800

    Change module in-8-81-bean-scope to a web-mvc project.

---
+++ thinking-in-spring/in-8-81-bean-scope/src/main/java/com/lzhlyle/spring/demo/bean/scope/web/WebConfiguration.java
@@ -0,0 +1,20 @@
+package com.lzhlyle.spring.demo.bean.scope.web;
+
+import com.lzhlyle.spring.demo.ioc.overview.domain.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class WebConfiguration {
+    @Bean
+    @RequestScope
+    public User user() {
+        User user = new User();
+        user.setId(234L);
+        user.setName("mvc");
+        return user;
+    }
+}
