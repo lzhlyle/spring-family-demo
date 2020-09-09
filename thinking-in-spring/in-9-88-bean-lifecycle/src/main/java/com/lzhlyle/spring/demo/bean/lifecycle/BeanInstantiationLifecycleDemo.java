c44325864923f49908fd8442a61abd15f47e35ef
lzhlyle <lzhlyle@msn.cn>
2020-09-09 15:30:03 +0800

    Append demo in module in-9-88-bean-lifecycle
    - BeanInstantiationLifecycleDemo

---
+++ thinking-in-spring/in-9-88-bean-lifecycle/src/main/java/com/lzhlyle/spring/demo/bean/lifecycle/BeanInstantiationLifecycleDemo.java
@@ -0,0 +1,34 @@
+package com.lzhlyle.spring.demo.bean.lifecycle;
+
+import com.lzhlyle.spring.demo.ioc.overview.domain.SuperUser;
+import com.lzhlyle.spring.demo.ioc.overview.domain.User;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.util.ObjectUtils;
+
+public class BeanInstantiationLifecycleDemo {
+    public static void main(String[] args) {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        beanFactory.addBeanPostProcessor(new MyInstantiationAwareBeanPostProcessor());
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        reader.loadBeanDefinitions("META-INF/dependency-lookup-context.xml");
+
+        User user = beanFactory.getBean("user", User.class);
+        System.out.println(user);
+
+        SuperUser superUser = beanFactory.getBean("superUser", SuperUser.class);
+        System.out.println(superUser);
+    }
+
+    static class MyInstantiationAwareBeanPostProcessor implements InstantiationAwareBeanPostProcessor {
+        @Override
+        public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
+            if (ObjectUtils.nullSafeEquals("superUser", beanName) && SuperUser.class.equals(beanClass)) {
+                return new SuperUser();
+            }
+            return null;
+        }
+    }
+}
