3424d5ae55f4f95c7a263a3494d1b14e1791e72b
lzhlyle <lzhlyle@msn.cn>
2020-09-09 14:42:07 +0800

    Append demo in module in-9-88-bean-lifecycle
    - MergeBeanDefinitionDemo

---
+++ thinking-in-spring/in-9-88-bean-lifecycle/src/main/java/com/lzhlyle/spring/demo/bean/lifecycle/MergeBeanDefinitionDemo.java
@@ -0,0 +1,20 @@
+package com.lzhlyle.spring.demo.bean.lifecycle;
+
+import com.lzhlyle.spring.demo.ioc.overview.domain.SuperUser;
+import com.lzhlyle.spring.demo.ioc.overview.domain.User;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+public class MergeBeanDefinitionDemo {
+    public static void main(String[] args) {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        reader.loadBeanDefinitions("META-INF/dependency-lookup-context.xml");
+
+        User user = beanFactory.getBean("user", User.class);
+        System.out.println(user);
+
+        SuperUser superUser = beanFactory.getBean("superUser", SuperUser.class);
+        System.out.println(superUser);
+    }
+}
