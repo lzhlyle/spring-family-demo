d506e0813b020fb5ea02f7bbc22aa1678638a010
lzhlyle <lzhlyle@msn.cn>
2020-11-05 09:44:07 +0800

    Update properties of spring-cloud projects.

--- spring-cloud/service-client/src/main/java/com/lzhlyle/spring/cloud/service/client/web/ConsumerController.java
+++ spring-cloud/service-client/src/main/java/com/lzhlyle/spring/cloud/service/client/web/ConsumerController.java
@@ -2,6 +2,7 @@ package com.lzhlyle.spring.cloud.service.client.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +10,9 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 @RestController
 public class ConsumerController {
+
+    @Autowired
+    private DiscoveryClient client;
 
     @Autowired
     private RestTemplate restTemplate;
